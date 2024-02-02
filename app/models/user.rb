d0cb76ffe783e384e42ac09b72eb18018833dd4c
Mikiko9882 <teddy.0802.t@gmail.com>
2024-02-02 21:42:39 +0900

    テスト作成機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  has_many :test_results, dependent: :destroy
+
   validates :first_name, presence: true, length: { maximum: 255 }
   validates :last_name, presence: true, length: { maximum: 255 }
   validates :nickname, presence: true
@@ -10,4 +12,12 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+
+  def own?(object)
+    id == object.user_id
+  end
+
+  def average_achievement_rate
+    test_results.average(:achievement_rate)
+  end
 end
